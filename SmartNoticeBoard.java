e31ba91e4ba0aa1cc73d718871444d7e5e4c3034
Abhisek Pandey <abhisek.pandey.1289@gmail.com>
2022-11-24 19:10:51 +0530

    .

--- SmartNoticeBoard.java
+++ SmartNoticeBoard.java
@@ -1,24 +1,24 @@
 import java.io.*;
+import java.net.InetAddress;
+import java.util.Date;
+import org.apache.commons.net.ntp.NTPUDPClient; 
+import org.apache.commons.net.ntp.TimeInfo;
 public class SmartNoticeBoard	{
 	//Declaring a File object to store the Notices in one folder
 	static final File Notices = new File("./Notices");
 	//Getting the list of all the Media file present in Notices folder and storring them ib ab Array
 	static File[] NoticesContent = null;
+	static String TIME_SERVER = "time-a.nist.gov";
+	static NTPUDPClient timeClient = new NTPUDPClient();
+           
 	public static void main(String[] args) throws Exception	{
-			while(true)	{
-				System.out.println("Updation and Arrangement");
-				SmartNoticeBoardUpdation.updation();
-				System.out.println("Updated");
-				FileArrangement.arrange();
-				System.out.println("Arranged");
-				Thread.sleep(5000);
-				int i = 1;
-				while(true)	{
-					System.out.println("Displaying");
-					NoticeDisplaying.Display();
-					if (i == 3 )break;
-					i++;
-					}
-				}
-			}
-		}
+		
+	}
+	public static long time() throws Exception	{
+		InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
+    	TimeInfo timeInfo = timeClient.getTime(inetAddress);
+    	long returnTime = timeInfo.getReturnTime();
+        Date time = new Date(returnTime);
+		return time.getTime();
+	}
+}
